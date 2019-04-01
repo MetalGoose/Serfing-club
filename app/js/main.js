38711b06ab8c3ba2e167773fa9f035dd495755a1
MetalGoose <42269983+MetalGoose@users.noreply.github.com>
2019-04-01 16:32:34 +0400

    Add libs: JQuery, OwlCarusel. Add header slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello Surfing!');
+$(document).ready(function(){
+
+	var headerSlider = $(".owl-carousel");
+
+	headerSlider.owlCarousel({
+		items: 1,
+		loop: true,
+		dots: false,
+		smartSpeed: 300,
+	});
+
+	$('#headerSliderLeft').click(function() {
+	    headerSlider.trigger('next.owl.carousel');
+	})
+
+	$('#headerSliderRight').click(function() {
+	    headerSlider.trigger('prev.owl.carousel');
+
+	headerSlider.on('changed.owl.carousel', function(event){
+		console.log(event.item.count)
+		console.log(event.item.index)
+	})
+})
+});
