25ea8536b1942d516aa4c343e9b7d956a734e401
MetalGoose <42269983+MetalGoose@users.noreply.github.com>
2019-04-02 08:44:20 +0400

    Header slider rework

--- app/js/main.js
+++ app/js/main.js
@@ -2,23 +2,33 @@ $(document).ready(function(){
 
 	var headerSlider = $(".owl-carousel");
 
+	headerSlider.on('changed.owl.carousel', function(event){
+		$('.slides-controls-number__active').text(event.item.index + 1);
+		$('.slides-controls-number__total').text(event.item.count);
+	})
+
+	headerSlider.on('initialize.owl.carousel', function(event){
+		$('.slides-controls-number__active').text(event.item.index + 1);
+		$('.slides-controls-number__total').text(event.item.count);
+	})
+
 	headerSlider.owlCarousel({
 		items: 1,
-		loop: true,
+		// loop: true,
 		dots: false,
 		smartSpeed: 300,
 	});
 
 	$('#headerSliderLeft').click(function() {
-	    headerSlider.trigger('next.owl.carousel');
+	    headerSlider.trigger('prev.owl.carousel');
 	})
 
 	$('#headerSliderRight').click(function() {
-	    headerSlider.trigger('prev.owl.carousel');
+	    headerSlider.trigger('next.owl.carousel');
 
-	headerSlider.on('changed.owl.carousel', function(event){
-		console.log(event.item.count)
-		console.log(event.item.index)
-	})
+
+
+
+
 })
 });
